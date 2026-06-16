d50ec14fc9f0c32e70dbe66bbf529f927f084e40
vuvannghiawork <vuvannghia.work@gmail.com>
2026-06-16 16:55:57 +0700

    add_elevenlabs_data

--- temp_e/elevenlabs_data.py
+++ temp_e/elevenlabs_data.py
@@ -16,9 +16,6 @@ def generate_greeting_text(name: str, gender: str) -> str:
     return f"Xin chào, {pronoun} là {name}. {pronoun.capitalize()} có thể giúp gì cho bạn về các vấn đề pháp luật?"
 
 
-# --- ĐỊNH NGHĨA CÁC LỚP (CLASSES) ---
-
-
 @dataclass
 class DataEngine:
     id: uuid.UUID
@@ -29,8 +26,8 @@ class DataEngine:
 
 @dataclass
 class DataVoice:
-    id: uuid.UUID
-    voice_id: str
+    voice_uuid: uuid.UUID
+    voice_code: str
     engine_id: uuid.UUID
     is_active: bool = True
 
@@ -40,8 +37,7 @@ class DataPersona:
     id: uuid.UUID
     name: str
     gender: str
-    tts_engine: str
-    voice_id: str
+    voice_uuid: uuid.UUID
     description: str
     avatar_url: str
     greeting_text: str
@@ -49,13 +45,11 @@ class DataPersona:
     is_active: bool = True
 
 
-# --- KHỞI TẠO DỮ LIỆU BẰNG CLASSES VÀ THÊM VÀO LIST VỚI APPEND ---
-
 SAMPLE_ENGINE_DATA = []
 SAMPLE_VOICE_DATA = []
 SAMPLE_PERSONA_DATA = []
 
-# 1. Tạo và append các đối tượng DataEngine
+
 engine_edge = DataEngine(
     id=uuid.UUID("a12e3456-789a-bcde-f012-3456789abcde"),
     code="edge_tts",
@@ -73,85 +67,95 @@ engine_elevenlabs = DataEngine(
 SAMPLE_ENGINE_DATA.append(engine_elevenlabs)
 
 
+engine_piper = DataEngine(
+    id=uuid.UUID("e12e3456-789a-bcde-f012-3456789abcde"),
+    code="piper_tts",
+    name="Piper TTS",
+    is_active=True,
+)
+SAMPLE_ENGINE_DATA.append(engine_piper)
+
+
 # 2. Tạo và append các đối tượng DataVoice (Sử dụng id từ đối tượng DataEngine đã tạo)
 voice_nam_minh = DataVoice(
-    id=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcde"),
-    voice_id="vi-VN-NamMinhNeural",
+    voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcde"),
+    voice_code="vi-VN-NamMinhNeural",
     engine_id=engine_edge.id,
     is_active=True,
 )
 SAMPLE_VOICE_DATA.append(voice_nam_minh)
 
 voice_hoai_my = DataVoice(
-    id=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd1"),
-    voice_id="vi-VN-HoaiMyNeural",
+    voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd1"),
+    voice_code="vi-VN-HoaiMyNeural",
     engine_id=engine_edge.id,
     is_active=True,
 )
 SAMPLE_VOICE_DATA.append(voice_hoai_my)
 
 voice_quoc_hung = DataVoice(
-    id=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd2"),
-    voice_id="IKne3meq5aSn9XLyUdCD",
+    voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd2"),
+    voice_code="IKne3meq5aSn9XLyUdCD",
     engine_id=engine_elevenlabs.id,
     is_active=True,
 )
 SAMPLE_VOICE_DATA.append(voice_quoc_hung)
 
 voice_duc_kien = DataVoice(
-    id=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd3"),
-    voice_id="pNInz6obpgDQGcFmaJgB",
+    voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd3"),
+    voice_code="pNInz6obpgDQGcFmaJgB",
     engine_id=engine_elevenlabs.id,
     is_active=True,
 )
 SAMPLE_VOICE_DATA.append(voice_duc_kien)
 
 voice_lan_anh = DataVoice(
-    id=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd4"),
-    voice_id="EXAVITQu4vr4xnSDxMaL",
+    voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd4"),
+    voice_code="EXAVITQu4vr4xnSDxMaL",
     engine_id=engine_elevenlabs.id,
     is_active=True,
 )
 SAMPLE_VOICE_DATA.append(voice_lan_anh)
 
 voice_thanh_truc = DataVoice(
-    id=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd5"),
-    voice_id="hpp4J3VqNfWAUOO0d1Us",
+    voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd5"),
+    voice_code="hpp4J3VqNfWAUOO0d1Us",
     engine_id=engine_elevenlabs.id,
     is_active=True,
 )
 SAMPLE_VOICE_DATA.append(voice_thanh_truc)
 
+voice_hung_dung = DataVoice(
+    voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd6"),
+    voice_code="cjVigY5qzO86Huf0OWal",
+    engine_id=engine_elevenlabs.id,
+    is_active=True,
+)
+SAMPLE_VOICE_DATA.append(voice_hung_dung)
 
 SAMPLE_VOICE_DATA.extend(
     [
         DataVoice(
-            id=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd6"),
-            voice_id="cjVigY5qzO86Huf0OWal",
+            voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd7"),
+            voice_code="CwhRBWXzGAHq8TQ4Fs17",
             engine_id=engine_elevenlabs.id,
             is_active=True,
         ),
         DataVoice(
-            id=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd7"),
-            voice_id="CwhRBWXzGAHq8TQ4Fs17",
+            voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd8"),
+            voice_code="FGY2WhTYpPnrIDTdsKH5",
             engine_id=engine_elevenlabs.id,
             is_active=True,
         ),
         DataVoice(
-            id=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd8"),
-            voice_id="FGY2WhTYpPnrIDTdsKH5",
+            voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd9"),
+            voice_code="Xb7hH8MSUJpSbSDYk0k2",
             engine_id=engine_elevenlabs.id,
             is_active=True,
         ),
         DataVoice(
-            id=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcd9"),
-            voice_id="Xb7hH8MSUJpSbSDYk0k2",
-            engine_id=engine_elevenlabs.id,
-            is_active=True,
-        ),
-        DataVoice(
-            id=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcda"),
-            voice_id="XrExE9yKIg1WjnnlVkGX",
+            voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abcda"),
+            voice_code="XrExE9yKIg1WjnnlVkGX",
             engine_id=engine_elevenlabs.id,
             is_active=True,
         ),
@@ -159,13 +163,45 @@ SAMPLE_VOICE_DATA.extend(
 )
 
 
-# 3. Tạo và append các đối tượng DataPersona (Sử dụng code của Engine và voice_id của Voice)
+voice_ban_mai = DataVoice(
+    voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abce0"),
+    voice_code="banmai",
+    engine_id=engine_piper.id,
+    is_active=True,
+)
+SAMPLE_VOICE_DATA.append(voice_ban_mai)
+
+voice_thanh_phuong = DataVoice(
+    voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abce1"),
+    voice_code="yannew",
+    engine_id=engine_piper.id,
+    is_active=True,
+)
+SAMPLE_VOICE_DATA.append(voice_thanh_phuong)
+
+voice_minh_quang = DataVoice(
+    voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abce2"),
+    voice_code="minhquang",
+    engine_id=engine_piper.id,
+    is_active=True,
+)
+SAMPLE_VOICE_DATA.append(voice_minh_quang)
+
+voice_manh_dung = DataVoice(
+    voice_uuid=uuid.UUID("c12e3456-789a-bcde-f012-3456789abce3"),
+    voice_code="duyoryx3175",
+    engine_id=engine_piper.id,
+    is_active=True,
+)
+SAMPLE_VOICE_DATA.append(voice_manh_dung)
+
+
+# 3. Tạo và append các đối tượng DataPersona (Sử dụng voice_uuid của Voice)
 persona_nam_minh = DataPersona(
     id=uuid.UUID("f0e747a0-48ee-4d60-9302-197a8e225673"),
     name="Nam Minh",
     gender="Nam",
-    tts_engine=engine_edge.code,
-    voice_id=voice_nam_minh.voice_id,
+    voice_uuid=voice_nam_minh.voice_uuid,
     description="Giọng nam miền Bắc trầm ấm, chững chạc.",
     avatar_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/images/vi-VN-NamMinhNeural-v1.png",
     greeting_text=generate_greeting_text("Nam Minh", "Nam"),
@@ -178,8 +214,7 @@ persona_hoai_my = DataPersona(
     id=uuid.UUID("d07e0f7a-07a5-4d7b-9abe-84d4fecaac59"),
     name="Hoài My",
     gender="Nữ",
-    tts_engine=engine_edge.code,
-    voice_id=voice_hoai_my.voice_id,
+    voice_uuid=voice_hoai_my.voice_uuid,
     description="Giọng nữ miền Nam nhẹ nhàng, truyền cảm.",
     avatar_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/images/vi-VN-HoaiMyNeural-v1.png",
     greeting_text=generate_greeting_text("Hoài My", "Nữ"),
@@ -192,8 +227,7 @@ persona_quoc_hung = DataPersona(
     id=uuid.UUID("e6db7128-586e-4d19-b1ad-d9bb58aa0dee"),
     name="Quốc Hùng",
     gender="Nam",
-    tts_engine=engine_elevenlabs.code,
-    voice_id=voice_quoc_hung.voice_id,
+    voice_uuid=voice_quoc_hung.voice_uuid,
     description="Giọng nam trầm ấm, tự tin và năng động.",
     avatar_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/images/20.jpg",
     greeting_text=generate_greeting_text("Quốc Hùng", "Nam"),
@@ -206,8 +240,7 @@ persona_duc_kien = DataPersona(
     id=uuid.UUID("b1e9982f-b3ea-4c91-ba94-ca5e1666e4d7"),
     name="Đức Kiên",
     gender="Nam",
-    tts_engine=engine_elevenlabs.code,
-    voice_id=voice_duc_kien.voice_id,
+    voice_uuid=voice_duc_kien.voice_uuid,
     description="Giọng nam chững chạc, uy quyền và kiên định.",
     avatar_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/images/2.jpg",
     greeting_text=generate_greeting_text("Đức Kiên", "Nam"),
@@ -220,22 +253,21 @@ persona_lan_anh = DataPersona(
     id=uuid.UUID("02b6d3d3-6f70-4eb2-850d-f6e8e40bd3b9"),
     name="Lan Anh",
     gender="Nữ",
-    tts_engine=engine_elevenlabs.code,
-    voice_id=voice_lan_anh.voice_id,
+    voice_uuid=voice_lan_anh.voice_uuid,
     description="Giọng nữ trưởng thành, đáng tin cậy và tự tin.",
-    avatar_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/images/16.jpg",
+    avatar_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/images/47.jpg",
     greeting_text=generate_greeting_text("Lan Anh", "Nữ"),
     greeting_audio_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/audio/EXAVITQu4vr4xnSDxMaL.mp3",
     is_active=True,
 )
 SAMPLE_PERSONA_DATA.append(persona_lan_anh)
 
+
 persona_thanh_truc = DataPersona(
     id=uuid.UUID("f6f3bf81-d5d8-44a1-9b5c-3bb3afb53c9d"),
     name="Thanh Trúc",
     gender="Nữ",
-    tts_engine=engine_elevenlabs.code,
-    voice_id=voice_thanh_truc.voice_id,
+    voice_uuid=voice_thanh_truc.voice_uuid,
     description="Giọng nữ chuyên nghiệp, tươi sáng và ấm áp.",
     avatar_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/images/15.jpg",
     greeting_text=generate_greeting_text("Thanh Trúc", "Nữ"),
@@ -243,3 +275,70 @@ persona_thanh_truc = DataPersona(
     is_active=True,
 )
 SAMPLE_PERSONA_DATA.append(persona_thanh_truc)
+
+
+persona_ban_mai = DataPersona(
+    id=uuid.UUID("f0e747a0-48ee-4d60-9302-197a8e225680"),
+    name="Ban Mai",
+    gender="Nữ",
+    voice_uuid=voice_ban_mai.voice_uuid,
+    description="Giọng nữ nhẹ nhàng, trong trẻo.",
+    avatar_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/images/26.jpg",
+    greeting_text=generate_greeting_text("Ban Mai", "Nữ"),
+    greeting_audio_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/audio/banmai.wav",
+    is_active=True,
+)
+SAMPLE_PERSONA_DATA.append(persona_ban_mai)
+
+persona_thanh_phuong = DataPersona(
+    id=uuid.UUID("f0e747a0-48ee-4d60-9302-197a8e225681"),
+    name="Thanh Phương",
+    gender="Nữ",
+    voice_uuid=voice_thanh_phuong.voice_uuid,
+    description="Giọng nữ truyền cảm, ấm áp.",
+    avatar_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/images/49.jpg",
+    greeting_text=generate_greeting_text("Thanh Phương", "Nữ"),
+    greeting_audio_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/audio/yannew.wav",
+    is_active=True,
+)
+SAMPLE_PERSONA_DATA.append(persona_thanh_phuong)
+
+persona_minh_quang = DataPersona(
+    id=uuid.UUID("f0e747a0-48ee-4d60-9302-197a8e225682"),
+    name="Minh Quang",
+    gender="Nam",
+    voice_uuid=voice_minh_quang.voice_uuid,
+    description="Giọng nam trẻ trung, năng động.",
+    avatar_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/images/39.jpg",
+    greeting_text=generate_greeting_text("Minh Quang", "Nam"),
+    greeting_audio_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/audio/minhquang.wav",
+    is_active=True,
+)
+SAMPLE_PERSONA_DATA.append(persona_minh_quang)
+
+persona_manh_dung = DataPersona(
+    id=uuid.UUID("f0e747a0-48ee-4d60-9302-197a8e225683"),
+    name="Mạnh Dũng",
+    gender="Nam",
+    voice_uuid=voice_manh_dung.voice_uuid,
+    description="Giọng nam trầm ấm, vững chãi.",
+    avatar_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/images/29.jpg",
+    greeting_text=generate_greeting_text("Mạnh Dũng", "Nam"),
+    greeting_audio_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/audio/duyoryx3175.wav",
+    is_active=True,
+)
+SAMPLE_PERSONA_DATA.append(persona_manh_dung)
+
+
+persona_hung_dung = DataPersona(
+    id=uuid.UUID("f0e747a0-48ee-4d60-9302-197a8e225690"),
+    name="Hùng Dũng",
+    gender="Nam",
+    voice_uuid=voice_hung_dung.voice_uuid,
+    description="Giọng nam trầm ấm, truyền cảm.",
+    avatar_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/images/79.jpg",
+    greeting_text=generate_greeting_text("Hùng Dũng", "Nam"),
+    greeting_audio_url=f"{elevenlabs_env.R2_PUBLIC_DOMAIN}/audio/cjVigY5qzO86Huf0OWal.mp3",
+    is_active=True,
+)
+SAMPLE_PERSONA_DATA.append(persona_hung_dung)
